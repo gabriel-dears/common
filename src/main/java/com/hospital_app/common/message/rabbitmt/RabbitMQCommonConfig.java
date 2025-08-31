d3e15dbaca3040414f4bd092697a3942b544af4a
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-08-31 02:31:52 -0300

    feat: Update submodules reference

---
+++ src/main/java/com/hospital_app/common/message/rabbitmt/RabbitMQCommonConfig.java
@@ -0,0 +1,15 @@
+package com.hospital_app.common.message.rabbitmt;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQCommonConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
